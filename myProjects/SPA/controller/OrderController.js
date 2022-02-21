9403d9f218452d80b9ce8b293b788872690a4494
RashindiPaulusha99 <rashindipaulusha837@gmail.com>
2022-02-21 19:56:37 +0530

    add keyup event sell quentity field

--- myProjects/SPA/controller/OrderController.js
+++ myProjects/SPA/controller/OrderController.js
@@ -21,6 +21,20 @@ $("#ids").click(function () {
 
 /*-------------------Item Sec-----------------------*/
 
+var regExSellQuantity=/^[0-9]{2,20}$/;
+
+$("#sellQty").keyup(function (event) {
+
+    let sellQty = $("#sellQty").val();
+    if (regExSellQuantity.test(sellQty)){
+        $("#sellQty").css('border','2px solid blue');
+        $("#errorSellQty").text("");
+    }else {
+        $("#sellQty").css('border','2px solid red');
+        $("#errorSellQty").text("Quantity is a required field: Pattern 00");
+    }
+});
+
 function loadItemCodes() {
     $("#codes").empty();
     for (var i = 0; i <itemDB.length ; i++) {
@@ -43,6 +57,105 @@ $("#codes").click(function (event) {
 /*-------------------Order Table-----------------------*/
 
 $("#btnAddCart").click(function () {
+
+    $("#tblItem tbody > tr").off("click");
+    //$("#tblItem tbody > tr").off("dblclick");
+
+    if($("#errorCode").text()!=""||$("#errorKind").text()!=""||$("#errorItemName").text()!=""||$("#errorQty").text()!=""||$("#errorPrice").text()!=""||
+        $("#itemCode").val()==""||$("#nameOfItem").val()==""||$("#kind").val()==""||$("#qty").val()==""||$("#unitPrice").val()==""){
+        $("#btnSaveItem").disable();
+    }else {
+
+        let text = "Do you really want to save this Item?";
+
+        if (confirm(text) == true) {
+            let itemCode = $("#itemCode").val();
+            let kind = $("#kind").val();
+            let itemName = $("#nameOfItem").val();
+            let qty = $("#qty").val();
+            let unitPrice = $("#unitPrice").val();
+
+            var itemDetails={
+                code:itemCode,
+                kind:kind,
+                name:itemName,
+                qty:qty,
+                price:unitPrice
+            }
+
+            var ifDuplicate=false;
+
+            var code=$("#itemCode").val();
+            var trim = $.trim(code);
+
+            for (var j = 0; j < itemDB.length; j++) {
+                if (trim == itemDB[j].code){
+                    ifDuplicate = true;
+                }else {
+                    ifDuplicate = false;
+                }
+            }
+
+            if (ifDuplicate == false){
+                itemDB.push(itemDetails);
+                $("#tblItem tbody").empty();
+
+                for (var i = 0; i < itemDB.length; i++) {
+                    let raw = `<tr><td> ${itemDB[i].code} </td><td> ${itemDB[i].kind} </td><td> ${itemDB[i].name} </td><td> ${itemDB[i].qty} </td><td> ${itemDB[i].price} </td></tr>`;
+                    $("#tblItem tbody").append(raw);
+                }
+
+                $("#itemCode").val("");
+                $("#kind").val("");
+                $("#nameOfItem").val("");
+                $("#qty").val("");
+                $("#unitPrice").val("");
+
+                $("#itemCode").css('border', '2px solid transparent');
+                $("#kind").css('border', '2px solid transparent');
+                $("#nameOfItem").css('border', '2px solid transparent');
+                $("#qty").css('border', '2px solid transparent');
+                $("#unitPrice").css('border', '2px solid transparent');
+            }else {
+                alert("Already Exists");
+            }
+
+        } else {
+
+        }
+    }
+
+    $("#tblItem tbody > tr").click(function () {
+
+        tblItemRow=$(this);
+
+        var code=tblItemRow.children(':nth-child(1)').text();
+        var trim1 = $.trim(code);
+        var kind=tblItemRow.children(':nth-child(2)').text();
+        var trim2 = $.trim(kind);
+        var iName=tblItemRow.children(':nth-child(3)').text();
+        var trim3 = $.trim(iName);
+        var qty=tblItemRow.children(':nth-child(4)').text();
+        var trim4 = $.trim(qty);
+        var price=tblItemRow.children(':nth-child(5)').text();
+        var trim5 = $.trim(price);
+
+        $("#itemCode").val(trim1);
+        $("#kind").val(trim2);
+        $("#nameOfItem").val(trim3);
+        $("#qty").val(trim4);
+        $("#unitPrice").val(trim5);
+    });
+
+    /*$("#tblItem tbody > tr").dblclick(function () {
+        $(this).remove();
+
+        $("#itemCode").val("");
+        $("#kind").val("");
+        $("#nameOfItem").val("");
+        $("#qty").val("");
+        $("#unitPrice").val("");
+    });*/
 
 
 });
